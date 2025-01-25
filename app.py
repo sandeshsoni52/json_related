8661b2ee290216b43c1b60bad90dbaccd0f9cd31
unknown <learnersolution2024@gmail.com>
2025-01-25 10:47:05 +0530

    only lowercase courses

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ data = {
             "undergraduate": {
                 "basicappliedscience": {
                     "Dr. G.Y. Pathrikar College of Computer Science & Information Technology": {
-                        "Courses": [
+                        "courses": [
                             "B Sc Hons. Computer Science",
                             "B Sc Hons. Information Technology",
                             "B Sc Hons. Science",
@@ -25,7 +25,7 @@ data = {
                         ]
                     },
                     "Institute of Bioscience and Technology": {
-                        "Courses": [
+                        "courses": [
                             "B Sc Hons. Biotechnology",
                             "B Sc Hons. Bioinformatics",
                             "B Sc Hons. Microbiology",
@@ -37,7 +37,7 @@ data = {
                         ]
                     },
                     "School of Basic & Applied Science": {
-                        "Courses": [
+                        "courses": [
                             "B Sc Hons. Physics",
                             "B Sc Hons. Chemistry",
                             "B Sc Hons. Mathematics",
@@ -53,7 +53,7 @@ data = {
                 },
                 "design": {
                     "Leonardo Da Vinci School of Design": {
-                        "Courses": [
+                        "courses": [
                             "BFA Applied Art",
                             "BFA Contemporary Art",
                             "BFA Traditional Art and Craft",
@@ -67,7 +67,7 @@ data = {
                 },
                 "engineering": {
                     "Jawaharlal Nehru Engineering College": {
-                        "Courses": [
+                        "courses": [
                             "B Tech Chemical Engineering",
                             "B Tech Civil Engineering",
                             "B Tech Computer Science and Engineering",
@@ -81,19 +81,19 @@ data = {
                         ]
                     },
                     "Institute of Fire Service Engineering": {
-                        "Courses": [
+                        "courses": [
                             "B Sc Fire and Safety"
                         ]
                     },
                     "University Department of Information and Communication Technology (UDICT)": {
-                        "Courses": [
+                        "courses": [
                             "B Tech Information Technology",
                             "B Tech AI & ML",
                             "B Tech Data Science"
                         ]
                     },
                     "School of Engineering & Technology": {
-                        "Courses": [
+                        "courses": [
                             "B Tech Agriculture Engineering",
                             "B Tech Computer Science & Engineering (IoT, Cyber Security)",
                             "B Tech Electrical and Instrumentation Engineering",
@@ -110,24 +110,24 @@ data = {
                         ]
                     },
                     "University Department of Pharmaceutical Sciences": {
-                        "Courses": [
+                        "courses": [
                             "Bachelor Pharmacy"
                         ]
                     }
                 },
                 "interdisciplinarystudies": {
                     "Department of Sports, Physical Education & Yog Science": {
-                        "Courses": [
+                        "courses": [
                             "B P E S Bachelor of Physical Education & Sports"
                         ]
                     },
                     "Department of Education": {
-                        "Courses": [
+                        "courses": [
                             "B A Hons. Education"
                         ]
                     },
                     "University Department of Music & Theatre": {
-                        "Courses": [
+                        "courses": [
                             "BPA Music",
                             "BPA Theatre"
                         ]
@@ -135,7 +135,7 @@ data = {
                 },
                 "management": {
                     "Institute of Management & Research": {
-                        "Courses": [
+                        "courses": [
                             "BBA Hons./ Hons. with Research",
                             "BBA Hons./ Hons. with Research in FinTech",
                             "BBA Hons./ Hons. with Research in Business Analytics",
@@ -143,14 +143,14 @@ data = {
                         ]
                     },
                     "hotelmanagement": {
-                        "Courses": [
+                        "courses": [
                             "B Sc Hons. Hotel Operations Catering Services",
                             "BBA Hons. in Aviation, Hospitality and Travel & Tourism",
                             "B Sc Hons. Culinary Arts"
                         ]
                     },
                     "Nath School of Business & Technology": {
-                        "Courses": [
+                        "courses": [
                             "Bachelor Management Studies (BMS)",
                             "BCA Cloud Technology & Information Security",
                             "MCA Digital Product Technology",
@@ -161,7 +161,7 @@ data = {
                 },
                 "performingarts": {
                     "MAHAGAMI GURUKUL Center for Performing Arts": {
-                        "Courses": [
+                        "courses": [
                             "BPA Kathak (Lab and Kathak)",
                             "BPA Odissi (Lab and Gurukul)"
                         ]
@@ -169,20 +169,20 @@ data = {
                 },
                 "socialscience": {
                     "College of Journalism & Mass Communication": {
-                        "Courses": [
+                        "courses": [
                             "B A International Journalism & Electronic Media",
                             "B A Hons./Reg. with Research in Mass Communication and Media"
                         ]
                     },
                     "Institute of Social Sciences": {
-                        "Courses": [
+                        "courses": [
                             "Bachelor Social Work (BSW)",
                             "B A Hons. Psychology",
                             "B A Hons. Economics"
                         ]
                     },
                     "School of Film Arts": {
-                        "Courses": [
+                        "courses": [
                             "B A Hons. Cinematography",
                             "B A Hons. Film Direction",
                             "B A Hons. Film Editing",
@@ -193,17 +193,17 @@ data = {
                         ]
                     },
                     "Department of Photography": {
-                        "Courses": [
+                        "courses": [
                             "B A Hons. Photography"
                         ]
                     },
                     "Institute of Indian & Foreign Languages": {
-                        "Courses": [
+                        "courses": [
                             "B A Hons. English"
                         ]
                     },
                     "School of Legal Studies & Research": {
-                        "Courses": [
+                        "courses": [
                             "Bachelor Law (LL.B.)",
                             "BBA Bachelor of Business Administration and Bachelor of Law"
                         ]
